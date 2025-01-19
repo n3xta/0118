9efe7b001eb43aafdc71187d77f07bdc31a7e661
nexta <gadressformuyuan@gmail.com>
2025-01-18 21:55:33 -0500

    palette

--- sketch.js
+++ sketch.js
@@ -4,19 +4,11 @@ let font;
 let squares = [];
 let numSquares = 500;
 
-const palette = [];
-
 function preload() {
   font = loadFont('assets/mintsoda.ttf');
 }
 
 function setup() {
-
-  let palette = [
-    color(0, 0, 255),
-    color(210),
-    color(0)
-  ];
 
   createCanvas(1600, 1600);
   pixelDensity(1);
@@ -25,16 +17,17 @@ function setup() {
   for (let i = 0; i < numSquares; i++) {
     let x = random(-width, 0);
     let y = random(-100, height + 100);
-    let size = random(10, 30);
+    let size = random(10, 40);
     let speed = random(5, 20);
     let yOffset = random(1000);
+    let gradientOffset = random(0, 1);
     squares.push({
       x: x,
       baseY: y,
       size: size,
       speed: speed,
       yOffset: yOffset,
-      c: random(palette),
+      gradientOffset: gradientOffset,
     });
   }
 }
@@ -45,6 +38,9 @@ function draw() {
 
   strokeCap(SQUARE);
   noStroke();
+
+  // stroke(200,176,255);
+  // strokeWeight(2);
 
   for (let i = 0; i < squares.length; i++) {
 
@@ -58,36 +54,45 @@ function draw() {
 
     let amplitude = 30;
 
-
     sq.y = sq.baseY + sin(frameCount * 0.05 + sq.yOffset) * amplitude;
 
     let sizeVariation = map(cos(frameCount * 0.02 + sq.yOffset), -1, 1, -5, 5);
     let currentSize = sq.size + sizeVariation;
     
-    fill(sq.c);
+    let t = map(sq.x, 0, width, 0, 1);
+    t = (t + sq.gradientOffset) % 1;
+
+    let c;
+    if (t < 0.25) {
+      c = lerpColor(color(240, 234, 210), color(221, 229, 182), t / 0.25);
+    } else if (t < 0.5 && t >= 0.25) {
+      c = lerpColor(color(221, 229, 182), color(173, 193, 120), (t - 0.25) / 0.25);
+    } else if (t < 0.75 && t >= 0.5) {
+      c = lerpColor(color(173, 193, 120), color(169, 132, 103), (t - 0.5) / 0.25);
+    } else {
+      c = lerpColor(color(169, 132, 103), color(108, 88, 76), (t - 0.75) / 0.25);
+    }
+
+    fill(c);
     
     rectMode(CENTER);
     rect(sq.x, sq.y, currentSize, currentSize);
   }
 
-  //   if (frameCount % 30 == 0) {
+    if (frameCount % 10 == 0) {
 
-  //   let fillColor = random([color(210), color(0)]);
-  //   fill(fillColor);
-  //   strokeWeight(20);
+    let fillColor = random([color(40, 54, 24), color(96, 108, 56)]);
+    fill(fillColor);
+    strokeWeight(20);
 
-  //   let numTitles = int(random(5, 20));
-  //   for (let i = 0; i < numTitles; i++) {
-  //     let a = random(100, 600);
-  //     let b = random(-width, width);
-  //     let c = random(-height, height);
+    let numTitles = int(random(5, 20));
+    for (let i = 0; i < numTitles; i++) {
+      let a = random(10, 300);
+      let b = random(-width, width);
+      let c = random(-height, height);
 
-  //     title(a, b, c);
-  //   }
-  // }
-
-  if (frameCount <= 300) {
-    saveCanvas('frames/frame-' + nf(frameCount, 4), 'png');
+      title(a, b, c);
+    }
   }
 
 }
@@ -97,7 +102,7 @@ function title(a, b, c) {
   textSize(random(a - 100, a + 100));
   textAlign(LEFT, TOP);
 
-  let strokeColor = random([color(210), color(0)]);
+  let strokeColor = random([color(40, 54, 24), color(96, 108, 56)]);
   stroke(strokeColor);
 
   text('WIND', random(b - 100, b + 100), random(c - 100, c + 100));
